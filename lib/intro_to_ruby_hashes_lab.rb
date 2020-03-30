db14e50972143433465eb2a573568a8b731ea6b1
Adao Natalino <sansonov@gmail.com>
2020-03-30 22:09:03 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,19 @@
 def new_hash
-  # return an empty hash
+  a = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  a = {:name => "Adao"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  a = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+a = {id: number}
 end
